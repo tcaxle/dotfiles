f60f9f1f281dbc72d8c69fbf67e64f8f98223b28
tom <tcac2@cam.ac.uk>
2018-12-06 17:39:22 +0000

    changed to MesloLGS font

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono for Powerline:pixelsize=12:antialias=true:autohint=true";
+static char *font = "MesloLGS Nerd Font:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
